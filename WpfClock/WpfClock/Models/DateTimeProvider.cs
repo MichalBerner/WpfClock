9e4beee7d02387c260510d2d3046b0e45bfd62a3
burn <burn@BURN-ZBOOK>
2019-02-22 19:28:00 +0100

    Base

---
+++ WpfClock/WpfClock/Models/DateTimeProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using WpfClock.Interfaces;
+
+namespace WpfClock.Models
+{
+	class DateTimeProvider : ModelBase, IDateTimeProvider
+	{
+		public DateTime CurrentDateTime { get; private set; } = DateTime.Now;
+
+		public DateTimeProvider()
+		{
+			var timer = new System.Windows.Threading.DispatcherTimer
+			{
+				Interval = TimeSpan.FromSeconds(1)
+			};
+			timer.Tick += (s, e) => CurrentDateTime = DateTime.Now;
+		}
+	}
+}
